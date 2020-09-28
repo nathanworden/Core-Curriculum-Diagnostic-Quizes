11fabf1a1e28b018563591d6ae3eaf3c097b6005
Nathan Worden <worden.nathan@gmail.com>
2020-09-27 23:10:08 -0700

    add quiz

---
+++ quiz1.js
@@ -0,0 +1,18 @@
+
+
+$(function() {
+  // Array.prototype.slice.call(document.querySelector('#main').querySelectorAll('.emphasis'));
+
+  // console.log(Array.prototype.slice.call((document.querySelector('#main')).querySelectorAll('.emphasis')));
+
+  // console.log($.makeArray($('#main').find('.emphasis')));
+
+
+
+  var value1 = document.querySelector('div').firstChild.nodeValue;
+  var value2 = document.querySelector('div').lastChild.textContent;
+  
+  console.log(value2 == value1);
+  console.log(typeof value2);
+  console.log(typeof value1);
+});
